de04a8a8e2de7e8b25dce80fcb84ae0c47e9d375
wert <wert@debian>
2016-03-07 15:59:03 -0300

    Stopclocks working

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,13 +1,26 @@
 'use strict';
 
 var controllerApp = angular.module('controllerApp', []);
-controllerApp.controller('ControllerCtrl', function($scope) {
-	$scope.list = []
+controllerApp.controller('ControllerCtrl', function($scope, $interval) {
+	$scope.list = {}
+	$scope.intervals = {}
 	$scope.add = function() {
-		$scope.list.push($scope.input);
+		$scope.list[$scope.input] = new Date(23*60*60*1000);
 	}
 	
-	$scope.remove = function(index) {
-		$scope.list.splice(index, 1);
+	$scope.remove = function(item) {
+		delete $scope.list[item];
 	}
+
+	$scope.start = function(item) {
+		$scope.intervals[item] = $interval(function(){ 
+			$scope.list[item] -= 1000; 
+		}, 1000);
+	}
+
+	$scope.stop = function(item) {
+		$interval.cancel($scope.intervals[item]);
+	}
+
+
 });
